60adb6257e496886a7fb1248be71d6135afe9b3a
oleksandr Kuklyshyn <oleksandr.kuklyshyn@gmail.com>
2019-04-15 20:56:36 +0300

    Create chat services

--- src/app/angular-bot/angular-bot.module.ts
+++ src/app/angular-bot/angular-bot.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { ChatComponent } from './chat/chat.component';
+import { ChatService } from './chat.service';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [MainComponent, ChatComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  providers: [
+    ChatService
   ]
 })
 export class AngularBotModule { }
--- src/app/angular-bot/chat/chat.component.ts
+++ src/app/angular-bot/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService, Message } from '../chat.service';
+
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  messages: Message[]= [];
+  value: string;
+  constructor(public chatService: ChatService) { }
 
   ngOnInit() {
+    this.chatService.conversation.subscribe((val) => {
+      this.messages = this.messages.concat(val);
+    });
   }
-
+  sendMessage() {
+    this.chatService.getBotAnswer(this.value);
+    this.value = '';
+  }
 }
